f3545b4a0abd452897481c7fe2ce1ce1290c2829
charlottesaidi <charlotte.saidi@outlook.fr>
2021-07-24 20:00:22 +0200

    minify css+js

--- assets/js/main.js
+++ assets/js/main.js
@@ -322,12 +322,3 @@ function validateForm() {
         return true;
     }
 }
-
-function loadStyleSheet(src) {
-    if (document.createStyleSheet){
-        document.createStyleSheet(src);
-    }
-    else {
-        $("head").append($("<link rel='stylesheet' href='"+src+" />"));
-    }
-};
