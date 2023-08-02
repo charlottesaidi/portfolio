cae1651db09b79ed93a228d0953ff65c81383371
Aliseth <charlotte.saidi@outlook.fr>
2023-08-02 19:45:55 +0200

    refonte layout auth & maj webpack-bundle

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -18,7 +18,23 @@ class SecurityController extends BaseController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+//        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('@EasyAdmin/page/login.html.twig', [
+            'error' => $error,
+            'last_username' => $lastUsername,
+            'page_title' => '',
+            'csrf_token_intention' => 'authenticate',
+            'username_label' => 'Adresse email',
+            'password_label' => 'Mot de passe',
+            'sign_in_label' => 'Se connecter',
+            'username_parameter' => 'email',
+            'password_parameter' => 'password',
+            'forgot_password_enabled' => true,
+            'forgot_password_path' => $this->generateUrl('app_forgot_password_request', ['...' => '...']),
+            'forgot_password_label' => 'Mot de passe oublié',
+            'remember_me_enabled' => true,
+            'remember_me_label' => 'Se souvenir de moi',
+        ]);
     }
 
     public function logout()
