de01af3ee2225cc4b12625aa31d42282bf00156a
Aliseth <charlotte.saidi@outlook.fr>
2024-04-29 16:32:01 +0200

    fix: fige le background

--- assets/javascript/module/bundles/initParticles.js
+++ assets/javascript/module/bundles/initParticles.js
@@ -18,7 +18,7 @@ export function loadParticleJs(element, options) {
             "value": options.nb,
             "density": {
             "enable": true,
-            "value_area": 1000
+            "value_area": 800
             }
         },
         "color": {
@@ -62,7 +62,7 @@ export function loadParticleJs(element, options) {
             "width": 1
         },
         "move": {
-            "enable": true,
+            "enable": false,
             "speed": options.speed,
             "direction": "none",
             "random": false,
